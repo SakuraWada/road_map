7fd91517f87a7b5b090624ef76161b1e7165ff80
SCP-wada <wada@scala-partners.com>
2024-05-07 10:50:04 +0900

    add register function

--- overwin/urls.py
+++ overwin/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import mypage,account_register_and_login
 
 app_name = "overwin"
--- road_map_products/urls.py
+++ road_map_products/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path("overwin/", include("overwin.urls")),
+    path("", include("overwin.urls")),
     path('admin/', admin.site.urls),
 ]
