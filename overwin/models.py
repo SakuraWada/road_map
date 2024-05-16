e594fb3e1a09f9eb4bdd8dbbb4fb8adf7e9d35d2
SCP-wada <wada@scala-partners.com>
2024-05-16 16:10:31 +0900

    close #23

--- overwin/models.py
+++ overwin/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class GamePlayer(models.Model):
-    battle_tag = models.CharField(max_length=32)
+    battle_tag = models.CharField(max_length=32, unique=True)
     def __str__(self):
         return self.battle_tag
 
@@ -13,7 +13,7 @@ class User(AbstractUser):
 
 class FavoriteGamePlayer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    game_player = models.ForeignKey(GamePlayer, on_delete=models.CASCADE)
+    game_player = models.ForeignKey(GamePlayer, on_delete=models.CASCADE, to_field='battle_tag')
 
     def __str__(self):
         return f"{self.user.username} - {self.game_player.battle_tag}"
