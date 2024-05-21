e10cfed3e6485e4503fc341b5a26fb4e38a950ad
SCP-wada <wada@scala-partners.com>
2024-05-21 14:04:30 +0900

    パーティー募集のモデル追加

--- overwin/models.py
+++ overwin/models.py
@@ -9,21 +9,47 @@ class GamePlayer(models.Model):
     def __str__(self):
         return self.battle_tag
 
+
 class User(AbstractUser):
     pass
 
+
 class FavoriteGamePlayer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user        = models.ForeignKey(User,       on_delete=models.CASCADE)
     game_player = models.ForeignKey(GamePlayer, on_delete=models.CASCADE, to_field='battle_tag')
 
     def __str__(self):
         return f"{self.user.username} - {self.game_player.battle_tag}"
 
+
 class Recruitment(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner              = models.ForeignKey(User, on_delete=models.CASCADE)
+
     max_recruit_member = models.IntegerField(
         validators=[
             MinValueValidator(1),
-            MaxValueValidator(4)
-        ]
+            MaxValueValidator(4),
+        ],
+        null=False,
+        blank=False
+    )
+
+    tank_role_num      = models.IntegerField(null=True)
+    damage_role_num    = models.IntegerField(null=True)
+    support_role_num   = models.IntegerField(null=True)
+    comment            = models.TextField(max_length=255, blank=True, null=True)
+
+
+class JoinedMember(models.Model):
+    recruitment        = models.ForeignKey(Recruitment, on_delete=models.CASCADE)
+    join_member        = models.ForeignKey(User,        on_delete=models.CASCADE)
+
+    ROLE_CHOICES       = models.TextChoices('ROLE_CHOICES', 'tank damage support')
+
+    role = models.CharField(
+        max_length=16,
+        choices=ROLE_CHOICES.choices,
+        default=None,
+        blank=True,
+        null=True
     )
