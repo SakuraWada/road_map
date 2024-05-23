23f2fb5db50bff3286c5fafd7a2797bf95a397bb
SCP-wada <wada@scala-partners.com>
2024-05-23 15:50:32 +0900

    urls.pyの修正

--- overwin/views/account_info.py
+++ overwin/views/account_info.py
@@ -2,18 +2,19 @@ from ..models import User
 from ..forms.account_register_and_login import UserRegisterForm
 from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views import generic
 from django.utils.decorators import method_decorator
 
-@login_required
-def show_account_info(request):
-    user = get_object_or_404(User, pk=request.user.pk)
-    context = {
-        'username': user.username,
-        'email': user.email,
-    }
-    return render(request, 'overwin/account_info.html', context)
+@method_decorator(login_required, name="dispatch")
+class AccountInfoView(generic.TemplateView):
+    template_name = 'overwin/account_info.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['username'] = self.request.user.username
+        context['email'] = self.request.user.email
+        return context
 
 @method_decorator(login_required, name="dispatch")
 class UpdateAccountInfoView(generic.UpdateView):
@@ -25,9 +26,12 @@ class UpdateAccountInfoView(generic.UpdateView):
         }
         return render(request, 'overwin/account_info_update.html', context)
 
-@login_required
-def delete_account(request):
-    user = get_object_or_404(User, pk=request.user.pk)
-    user.delete()
-    return redirect(reverse('overwin:login'))
+@method_decorator(login_required, name="dispatch")
+class DeleteAccountInfoView(generic.DeleteView):
+    model = User
+    success_url = reverse_lazy('overwin:login')
+    template_name = 'overwin/account_delete.html'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(User, pk=self.request.user.pk)
 
