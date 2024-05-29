d34396966e08ce618913b7e7a3b332e2285bcafc
SCP-wada <wada@scala-partners.com>
2024-05-29 14:02:29 +0900

    デザインの適用

--- overwin/forms/account_register_and_login.py
+++ overwin/forms/account_register_and_login.py
@@ -2,6 +2,9 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from ..models import User
 
 class UserRegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = " "
     class Meta:
         model = User
         fields = ('username', 'email')
