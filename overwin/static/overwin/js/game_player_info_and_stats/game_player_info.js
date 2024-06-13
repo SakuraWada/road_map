0648adc49f41170c8458113b471954161cebeb83
SCP-wada <wada@scala-partners.com>
2024-06-13 17:46:05 +0900

    グラフの大きさ、位置調整

--- overwin/static/overwin/js/game_player_info_and_stats/game_player_info.js
+++ overwin/static/overwin/js/game_player_info_and_stats/game_player_info.js
@@ -1,57 +1,67 @@
-const canvas = document.querySelector('canvas');
-const winRate = canvas.getAttribute('win-rate');
-var ctx = document.getElementById('win-rate-chart-1').getContext('2d');
+var canvasElemants = document.querySelectorAll('canvas[id^="win-rate-chart-"]');
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-    labels: [''],
-    datasets: [{
-            label: '',
-            data: [winRate],
-            backgroundColor: '#E0612B',
-            borderWidth: 0,
-        },
-        {
-            label: '',
-            data: [100-winRate],
-            backgroundColor: '#1D2539',
-            borderWidth: 0,
-        }
-    ],
-    },
-    options: {
-        indexAxis: 'y',
-        plugins: {
-            legend: {
-                display: false
+canvasElemants.forEach(function(canvas){
+
+    var ctx = canvas.getContext('2d');
+    const winRate = canvas.getAttribute('win-rate');
+    ctx.canvas.height="100%";
+
+    var WinRateChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: [''],
+        datasets: [{
+                label: '',
+                data: [winRate],
+                backgroundColor: '#E0612B',
+                borderWidth: 0,
             },
-            tooltip: {
-                enabled: false
-            },
-        },
-        scales: {
-            x: {
-                max: 100,
-                stacked: true,
-                display: false,
-                ticks: {
-                    display: false
-                },
-                grid: {
-                    display: false
-                },
-            },
-            y: {
-                stacked: true,
-                display: false,
-                ticks: {
-                    display: false
-                },
-                grid: {
-                    display: false
-                },
+            {
+                label: '',
+                data: [100-winRate],
+                backgroundColor: '#1D2539',
+                borderWidth: 0,
             }
+        ],
         },
-    },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            indexAxis: 'y',
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                },
+            },
+            scales: {
+                x: {
+                    max: 100,
+                    stacked: true,
+                    display: false,
+                    ticks: {
+                        display: false
+                    },
+                    grid: {
+                        display: false
+                    },
+                },
+                y: {
+                    stacked: true,
+                    display: false,
+                    ticks: {
+                        display: false
+                    },
+                    grid: {
+                        display: false
+                    },
+                }
+            },
+        },
+    });
+
+    WinRateChart.canvas.parentNode.style.height = '40px';
+    WinRateChart.canvas.parentNode.style.width = '110px';
 });
